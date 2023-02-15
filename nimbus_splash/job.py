2fb4e1d4df4fc24a289502d452dd2301d9f6112b
Jon Kragskow <jonkragskow@gmail.com>
2023-02-15 11:15:46 +0000

    feat: add xyzfile, ncores, maxcore checking, add trap for eviction, fail, and time limit, add supported nodes

--- nimbus_splash/job.py
+++ nimbus_splash/job.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from .utils import red_exit
+
 
 def write_file(input_file: str, node_type: str, time: str,
                verbose: bool = False) -> str:
@@ -90,6 +92,18 @@ def write_file(input_file: str, node_type: str, time: str,
         j.write('# UCX transport protocols for MPI\n')
         j.write('export UCX_THS=self,tcp,sm\n\n')
 
+        j.write('# If sigterm (eviction) copy files before job is killed\n')
+        j.write('trap "rsync -aP --exclude=*.tmp $localscratch/*')
+        j.write('$campaigndir; exit 15" 15\n')
+
+        j.write('# If node dies copy files before job is killed\n')
+        j.write('trap "rsync -aP --exclude=*.tmp $localscratch/*')
+        j.write('$campaigndir; exit 1" 1\n')
+
+        j.write('# If time limit reached, copy files before job is killed\n')
+        j.write('trap "rsync -aP --exclude=*.tmp $localscratch/*')
+        j.write('$campaigndir; exit 9" 9\n')
+
         j.write('# run the calculation and clean up\n')
         j.write('$(which orca) $input >> $campaigndir/$output\n\n')
 
@@ -105,7 +119,7 @@ def write_file(input_file: str, node_type: str, time: str,
     return job_file
 
 
-def check_envvar(var_str):
+def check_envvar(var_str: str) -> None:
     """
     Checks specified environment variable has been defined, exits program if
     variable is not defined
@@ -115,11 +129,92 @@ def check_envvar(var_str):
     var_str : str
         String name of environment variable
 
+    Returns
+    -------
+    None
     """
 
     try:
         os.environ[var_str]
     except KeyError:
         sys.exit("Please set ${} environment variable".format(var_str))
+
+    return
+
+
+def check_input_contents(file: str, n_cores: int, max_mem: int) -> str:
+    """
+    Checks contents of input file.
+    Specifically:
+        If specified xyz file exists
+        If maxcore (memory) specified is appropriate
+        If specified number of cores matches number on node
+
+    Parameters
+    ----------
+    var_str : str
+        String name of environment variable
+
+    Returns
+    -------
+    None
+    """
+
+    # Found number of cores definition
+    pal_found = False
+
+    # Found memory definition
+    mem_found = False
+
+    with open(file, 'r') as f:
+        for line in f:
+
+            # xyz file
+            if 'xyzfile' in line.lower():
+                xyzfile = line.split()[-1]
+                if not os.path.exists(xyzfile):
+                    red_exit("Error: specified xyz file does not exist")
+
+            # Number of cores
+            if '%pal nprocs' in line.lower():
+                pal_found = True
+
+                if len(line.split()) != 4:
+                    red_exit("Incorrect %PAL definition")
+
+                try:
+                    n_try = int(line.split()[-2])
+                except ValueError:
+                    red_exit(
+                        "Cannot parse number of cores in input file"
+                    )
+                if n_try != n_cores:
+                    red_exit(
+                        "Number of cores in input does not match node"
+                    )
+
+            # Number of cores
+            if '%maxcore' in line.lower():
+                mem_found = True
+
+                if len(line.split()) != 2:
+                    red_exit("Incorrect %maxcore definition")
+
+                try:
+                    n_try = int(line.split()[-1])
+                except ValueError:
+                    red_exit(
+                        "Cannot parse per core memory in input file"
+                    )
+                if n_try > max_mem:
+                    red_exit(
+                        "Specified per core memory in input exceeds node limit"
+                    )
+
+    if not pal_found:
+        red_exit("Cannot locate %PAL definition in input file")
+
+    if not mem_found:
+        red_exit("Cannot locate %maxcore definition in input file")
 
     return
